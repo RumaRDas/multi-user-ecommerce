8d77f93ac0e9593f3ef8f7bd23fb54589eff4013
Ruma Das <ruma@neo-analytics.com,au>
2025-03-12 00:22:39 +1030

    Adding function for getting register page value

--- frontend/dashboard/src/views/auth/Register.jsx
+++ frontend/dashboard/src/views/auth/Register.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 const Register = () => {
+  const [state, setState] = useState({ name: "", email: "", password: "" });
+
+  const inputHandle = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
       <div className="w-[350px] text-[#ffffff] p-2">
@@ -11,10 +23,12 @@ const Register = () => {
           <p className="text-sm mb-3 font-medium ">
             Please register your account
           </p>
-          <form>
+          <form onSubmit={submit}>
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="name">Name</label>
               <input
+                onChange={inputHandle}
+                value={state.name}
                 className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
                 type="text"
                 name="name"
@@ -26,6 +40,8 @@ const Register = () => {
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="email">Email</label>
               <input
+                onChange={inputHandle}
+                value={state.email}
                 className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
                 type="email"
                 name="email"
@@ -37,6 +53,8 @@ const Register = () => {
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="password">Password</label>
               <input
+                onChange={inputHandle}
+                value={state.password}
                 className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
                 type="password"
                 name="password"
